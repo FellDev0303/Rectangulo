4ee87f99683c201ef2b91004422bc5c2f7d1ea5c
FellDev0303 <josejose2868@hotmail.com>
2025-10-17 13:20:17 -0500

    Se crea la clse Figura.java con fines de herencia para Rectángulo

---
+++ src/main/java/Modelo/Figura.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author LENOVO
+ */
+public abstract class Figura {
+
+    protected double x1, y1; // vértice fijo
+    protected double x2, y2; // vértice variable
+
+    public Figura(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public double getX1() {
+        return x1;
+    }
+
+    public double getY1() {
+        return y1;
+    }
+
+    public double getX2() {
+        return x2;
+    }
+
+    public double getY2() {
+        return y2;
+    }
+
+    public void setX2(double x2) {
+        this.x2 = x2;
+    }
+
+    public void setY2(double y2) {
+        this.y2 = y2;
+    }
+
+
+}
